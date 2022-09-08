00cb85ab541e99a5a857c25ff87ab09a1f9f559d
frez_freedom <frez1374@gmail.com>
2022-09-08 18:30:35 +0430

    Add error messages

--- PasswordValidation.py
+++ PasswordValidation.py
@@ -4,5 +4,10 @@ from ValidationResult import ValidationResult
 
 class PasswordValidator:
 
+    SIZE_ERROR: str = 'Password must be at least 8 characters'
+    TWO_NUMBERS_ERROR: str = 'The password must contain at least 2 numbers'
+    CAPITAL_CHARACTER_ERROR: str = 'password must contain at least one capital letter'
+    SPECIAL_CHARACTER_ERROR: str = 'password must contain at least one special character'
+    
     def validate(validationData: ValidationData) -> ValidationResult:
         return ValidationResult(False, [])

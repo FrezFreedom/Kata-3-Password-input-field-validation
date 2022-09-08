da91c9a8573927254c6431fcf53e3084a9ad6366
frez_freedom <frez1374@gmail.com>
2022-09-08 18:07:04 +0430

    Change output of getError function and solve error spelling problem

--- ValidationResult.py
+++ ValidationResult.py
@@ -1,14 +1,14 @@
 
 class ValidationResult:
     __valid: bool;
-    __erorrMessages: list;
+    __errorMessages: list;
 
-    def __init__(valid: bool = False, erorrMessages: list = []):
+    def __init__(valid: bool = False, errorMessages: list = []):
         this.__valid = valid
-        this.__erorrMessages = erorrMessages
+        this.__errorMessages = errorMessages
 
     def isValid():
         return self.__valid
 
-    def getErorrMessages():
-        return __erorrMessages
+    def getErrorMessages():
+        return '\n'.join(self.__errorMessages)
